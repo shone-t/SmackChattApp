ae7845b167f4e925aff0226ddcd1812289f227ab
Shone <shonesrbija@yahoo.com>
2019-07-09 15:32:33 +0200

    pocetak rada sa porukama za kanal

---
+++ Smack/Model/Message.swift
@@ -0,0 +1,20 @@
+//
+//  Message.swift
+//  Smack
+//
+//  Created by MacBook Pro on 7/9/19.
+//  Copyright © 2019 MacBook Pro. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    
+    public private(set) var message : String!
+    public private(set) var userName : String!
+    public private(set) var channelID : String!
+    public private(set) var userAvatar : String!
+    public private(set) var userAvatarColor : String!
+    public private(set) var id : String!
+    public private(set) var timeStamp : String!
+}
--- Smack/Services/MessageService.swift
+++ Smack/Services/MessageService.swift
@@ -15,51 +15,51 @@ class MessageService {
     static let instance = MessageService()
     
     var channels = [Channel]()
-    
+    var messages = [Message]()
     var selectedChannel : Channel?
     
     func findAllChannel(completion: @escaping CompletionHandler) {
-
-//        ovo sam morao da dodam bilo koji da bi radilo
-//                let body: [String: Any] = [
-//            "name": "drugi nasilno dodat",
-//            "description": "opis drugog"
-//        ]
-//        Alamofire.request(URL_ADD_CHANNELS, method: .post, parameters: body, encoding: JSONEncoding.default, headers: BEARER_HEADER).responseJSON { (response) in
-//
-//            if response.result.error == nil {
-//                print("proslo dodavanje")
-//            } else {
-//                print("nije proslo",response.result.error as? String)
-//            }
-//        }
+        
+        //        ovo sam morao da dodam bilo koji da bi radilo
+        //                let body: [String: Any] = [
+        //            "name": "drugi nasilno dodat",
+        //            "description": "opis drugog"
+        //        ]
+        //        Alamofire.request(URL_ADD_CHANNELS, method: .post, parameters: body, encoding: JSONEncoding.default, headers: BEARER_HEADER).responseJSON { (response) in
+        //
+        //            if response.result.error == nil {
+        //                print("proslo dodavanje")
+        //            } else {
+        //                print("nije proslo",response.result.error as? String)
+        //            }
+        //        }
         
         Alamofire.request(URL_GET_CHANNELS, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: BEARER_HEADER).responseJSON { (response) in
             
             if response.result.error == nil {
                 guard let data = response.data else { return }
-// decodable JSON decoder ide sa do catch blokom
+                // decodable JSON decoder ide sa do catch blokom
                 
-//                do {
-//                    self.channels = try JSONDecoder().decode([Channel].self, from: data)
-//                } catch let error {
-//                    debugPrint(error as Any)
-//                }
-//                print(self.channels)
-// tradicionalni nacin
+                //                do {
+                //                    self.channels = try JSONDecoder().decode([Channel].self, from: data)
+                //                } catch let error {
+                //                    debugPrint(error as Any)
+                //                }
+                //                print(self.channels)
+                // tradicionalni nacin
                 if let json = try! JSON(data: data).array {
                     for item in json {
                         let name = item["name"].stringValue
                         let channelDescription = item["description"].stringValue
                         let id = item["_id"].stringValue
-
+                        
                         let channel = Channel(channelTitle: name, channelDescription: channelDescription, id: id)
                         //let channel = Channel(_id: id, name: name, description: channelDescription, __v: 1)
                         self.channels.append(channel)
                     }
-//                    print(self.channels)
-//                    print(self.channels[0].channelTitle)
-//                    print("prosao ovo 333")
+                    //                    print(self.channels)
+                    //                    print(self.channels[0].channelTitle)
+                    //                    print("prosao ovo 333")
                     NotificationCenter.default.post(name: NOTIF_CHANNELS_LOADED, object: nil)
                     completion(true)
                 }
@@ -69,6 +69,43 @@ class MessageService {
             }
         }
     }
+    
+    func findAllMessageForChannel(channelId: String, completition: @escaping CompletionHandler) {
+        
+        Alamofire.request("\(URL_GET_MESSAGES)\(channelId)", method: .get, parameters: nil, encoding: JSONEncoding.default, headers: BEARER_HEADER).responseJSON { (response) in
+            if response.result.error == nil {
+                self.clearMessages()
+                guard let data = response.data else {return}
+                if let json = try! JSON(data: data).array {
+                    for item in json {
+                        let messageBody = item["messageBody"].stringValue
+                        let channelId = item["channelId"].stringValue
+                        let id = item["_id"].stringValue
+                        let userName = item["userName"].stringValue
+                        let userAvatar = item["userAvatar"].stringValue
+                        let userAvatarColor = item["userAvatarColor"].stringValue
+                        let timeStamp = item["timeStamp"].stringValue
+                        
+                        let message = Message(message: messageBody, userName: userName, channelID: channelId, userAvatar: userAvatar, userAvatarColor: userAvatarColor, id: id, timeStamp: timeStamp)
+                        
+                        self.messages.append(message)
+                        
+                    }
+                    print(self.messages)
+                    completition(true)
+                }
+            } else {
+                debugPrint(response.result.error as Any)
+                completition(false)
+            }
+        }
+        
+        
+    }
+    func clearMessages() {
+        messages.removeAll()
+    }
+    
     func clearChannels(){
         channels.removeAll()
     }
